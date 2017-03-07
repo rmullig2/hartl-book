77ecd14f7f7d45ef0b3ab5e91f4381d5d3bc7111
Ray Mulligan <rmullig2@optonline.net>
2017-03-06 22:23:05 -0500

    Completed 8.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  
   get 'users/new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -11,5 +12,7 @@ Rails.application.routes.draw do
   post '/signup',  to: 'users#create'
   resources :users
 
-
+  get 'login', to:'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
